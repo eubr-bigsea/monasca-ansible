b73bdc8412b7ab2790b94930d97a156da9e20def
Flávio Ramalho <flaviosr@lsd.ufcg.edu.br>
2016-11-23 14:12:59 +0000

    Updated to reflect changes on the monasca project

--- roles/os_monasca/files/install_grafana.sh
+++ roles/os_monasca/files/install_grafana.sh
@@ -3,17 +3,20 @@
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=`pwd`
 
-go get -d github.com/grafana/grafana/...
+mkdir -p $GOPATH/src/github.com/grafana
 cd $GOPATH/src/github.com/grafana
-sudo rm -r grafana
 cp -r /opt/cloned-repos/grafana .
 cd grafana
 cp -r /opt/cloned-repos/grafana-plugins/datasources/monasca ./public/app/plugins/datasource/
 cp /opt/cloned-repos/monasca-ui/grafana-dashboards/* ./public/dashboards/
+
 go run build.go setup
-$GOPATH/bin/godep restore
-go run build.go build
+$GOPATH/bin/godep go run build.go build
+
+source /root/.nvm/nvm.sh
+nvm install 4.0.0
+nvm use 4.0.0
 npm config set unsafe-perm true
 npm install
-sudo npm install -g grunt-cli
+npm install -g grunt-cli
 grunt --force
